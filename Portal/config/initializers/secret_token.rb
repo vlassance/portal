2446ed458fe8b5616a40492e60b0926cc9b15130
Caio <caiotuxo@gmail.com>
2013-02-20 15:13:41 -0300

    rollback

--- Portal/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Portal::Application.config.secret_token = 'd9286219faf82d79be522c5bc26535b25f2907c6d505fce4f306b81449b012f0f9f4fe17bc6b46369b488e432ddfed144a966fef9a3fe74cb21358568c272872'
