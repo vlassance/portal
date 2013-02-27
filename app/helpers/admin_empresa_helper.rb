57b8120af43b87de71032661034148e579095cd1
Caio <caiotuxo@gmail.com>
2013-02-27 17:38:21 -0300

    helper

--- app/helpers/admin_empresa_helper.rb
+++
@@ -1,2 +0,0 @@
-module AdminEmpresaHelper
-end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,49 @@
 module ApplicationHelper
+ 
+  def layout_edit_and_new (options)
+    html = <<-HTML
+    
+          <div class="container-fluid">
+
+           <div class="row-fluid">
+             <div class="span12">
+
+          <div class="box bordered">
+            <div class="box-header">
+              <i class="icon-th-list"></i>#{ options[:title].to_s }
+              <div class="pull-right">
+                <div class="btn-group">
+                  <a class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="icon-cog"></i>
+                  </a>
+                  <ul class="dropdown-menu dropdown-left">
+                    <li><a href="#">Salvar</a></li>
+                    <li><a href="#">Cancelar</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+        <div class="row-fluid">
+
+          <div class="span12">
+            <div class="padded">
+              <div class="section-title">
+                #{ options[:subtitle].to_s }
+              </div>
+                   #{ render 'form' }
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+         </div>
+       </div>
+      </div>
+    HTML
+        html.html_safe
+  end
+  
  def icons
     File.readlines('icons.txt')
   end
