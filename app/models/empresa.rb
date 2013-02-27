6b46474c3b54e2ea7655b689cb9e9a60c4bd7608
Caio <caiotuxo@gmail.com>
2013-02-27 17:10:32 -0300

    index e form

--- app/models/empresa.rb
+++ app/models/empresa.rb
@@ -1,8 +1,4 @@
 class Empresa < Lugar
    
-  field :pais, type: String
-  field :descricao, type: String
-  field :site, type: String
-
   has_one :admin_empresa
 end
--- app/models/lugar.rb
+++ app/models/lugar.rb
@@ -1,11 +1,14 @@
 class Lugar
   include Mongoid::Document
-  field :nome
-  field :endereco
-  field :cep
-  field :cidade
-  field :estado
-  field :telefone
-  field :email
+  field :nome, type: String
+  field :endereco, type: String
+  field :cep, type: String
+  field :cidade, type: String
+  field :estado, type: String
+  field :telefone, type: String
+  field :email, type: String
+  field :pais, type: String
+  field :descricao, type: String
+  field :site, type: String
 
 end
