479f5cff2ac03585bcbd87281695d855e954f4de
Caio <caiotuxo@gmail.com>
2013-03-06 15:54:51 -0300

    notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def redirect_success(message,controller, action)
+  	redirect_to({:controller=> controller, :action => action}, :flash => { :notice_success => message })   
+  end
+
+  def redirect_error(message,controller, action)
+  	redirect_to({:controller=> controller, :action => action}, :flash => { :notice_error => message })   
+  end
+
 end
