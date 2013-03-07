373d6dc6800d35bfaf7f66a1e711f0137f2103aa
Caio <caiotuxo@gmail.com>
2013-03-07 12:43:28 -0300

    validations

--- config/initializers/client_side_validations.rb
+++ config/initializers/client_side_validations.rb
@@ -3,7 +3,7 @@
 
 #Uncomment the following block if you want each input field to have the validation messages attached.
  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
- 	puts html_tag.to_s
+
    unless html_tag =~ /^<label/
      %{<div class="input">#{html_tag}<span class="input-error" data-original-title=""> Error <i class="icon-exclamation-sign"></i>#{instance.error_message.first}</span></div>}.html_safe
    else
