6a19b2bca17b81fcefee451f7bdfc3931f06406d
Caio <caiotuxo@gmail.com>
2013-03-13 16:11:28 -0300

    login e geocode

--- app/models/admin_instituicao.rb
+++ app/models/admin_instituicao.rb
@@ -1,9 +1,2 @@
-class AdminInstituicao
-  include Mongoid::Document
-  field :nome, type: String
-  field :email, type: String
-  field :telefone, type: String
-  field :dataNascimento, type: Date
-  field :rg, type: String
-  field :cpf, type: String
+class AdminInstituicao < Usuario
 end
--- app/models/grupo.rb
+++ app/models/grupo.rb
@@ -4,15 +4,18 @@ class Grupo
   field :internal_id, :type => Integer
   field :nome, :type => String
   has_many :usuarios
-  has_many :menus
+  #has_many :menus
   
   validates_uniqueness_of :nome,:case_sensitive => true, :message => "nome ja existente"
   validates_presence_of :nome, :message => "digite um nome"
   
   
   #constantes
-  Grupo::ADMIN = '1'
-  # na tela de criar grupos, o internal_id esta considerando que os grupos seguem a ordem crescente...
-  # definir se criar grupos sera via CRUD ou seeds.
+  Grupo::ADMIN_INSTITUICAO = '1'
+  Grupo::COORDERNADOR = '2'
+  Grupo::ADMIN_EMPRESA = '3'
+  Grupo::GESTOR = '4'
+  Grupo::ALUNO = '5'
+
   
 end
