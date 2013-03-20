a3605336da7eee5ddc5b3f4723be48273d178a93
Caio <caiotuxo@gmail.com>
2013-03-20 17:39:23 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,9 @@
 #Menu.create(:nome => 'Usuarios', :url => '/users')
 #Menu.create(:nome => 'Grupos de Acesso', :url => '/grupos')
 HistoricoEstagio.destroy_all
+Evento.destroy_all
+Vaga.destroy_all
+Empresa.destroy_all
 HistoricoEstagio.create(modulo: "1", nota: 10)
 HistoricoEstagio.create(modulo: "2", nota: 9)
 HistoricoEstagio.create(modulo: "3", nota: 10)
@@ -34,8 +37,44 @@ puts "Grupos criados com sucesso"
 puts "Criando Usuario Administrador"
 	user = AdminInstituicao.create(:name => 'Administrador ', login: 'admin', :email => 'admin@gmail.com', :password => '12345678', :password_confirmation => '12345678')
 	user.grupo = admin
-	if user.save 
+	if user.save(validate: false)
 	puts "Usuario administrador criado com sucesso! -> login: admin@gmail.com, senha: 12345678"
+else
+	puts "*****nao foi possivel criar um usuario, por favor execute db:seed novamente" + user.errors.to_json
+end
+
+puts "Criando Usuario Coordenador"
+	user = CoordenadorEstagio.create(:name => 'Coordenador ', login: 'coordenador', :email => 'coordenador@gmail.com', :password => '12345678', :password_confirmation => '12345678')
+	user.grupo = coordenador
+	if user.save(validate: false)
+	puts "Usuario coordenador criado com sucesso! -> login: coordenador@gmail.com, senha: 12345678"
+else
+	puts "*****nao foi possivel criar um usuario, por favor execute db:seed novamente" + user.errors.to_json
+end
+
+puts "Criando Usuario Aluno"
+	user = Aluno.create(:name => 'Aluno ', login: 'aluno', :email => 'aluno@gmail.com', :password => '12345678', :password_confirmation => '12345678')
+	user.grupo = aluno
+	if user.save(validate: false)
+	puts "Usuario aluno criado com sucesso! -> login: aluno@gmail.com, senha: 12345678"
+else
+	puts "*****nao foi possivel criar um usuario, por favor execute db:seed novamente" + user.errors.to_json
+end
+
+puts "Criando Empresa"
+	empresa = Empresa.create(:name => 'Software Express')
+	if empresa.save(validate: false)
+	puts "empresa criada com sucesso"
+else
+	puts "*****nao foi possivel criar uma empresa, por favor execute db:seed novamente" + empresa.errors.to_json
+end
+
+puts "Criando Admin Empresa"
+	user = AdminEmpresa.create(:name => 'AdminEmpresa ', login: 'empresa', :email => 'empresa@gmail.com', :password => '12345678', :password_confirmation => '12345678')
+	user.grupo = admin_empresa
+	user.empresa = empresa
+	if user.save(validate: false)
+	puts "Usuario admin empresa criado com sucesso! -> login: empresa@gmail.com, senha: 12345678"
 else
 	puts "*****nao foi possivel criar um usuario, por favor execute db:seed novamente" + user.errors.to_json
 end
