e945b40da79e45cb5579659dd51fa4f05e75070a
João Guilherme Bersani Calice <jgcalice@gmail.com>
2013-03-27 15:34:43 -0300

    tela de inicio

--- app/controllers/contatos_importantes_controller.rb
+++ app/controllers/contatos_importantes_controller.rb
@@ -2,7 +2,6 @@ class ContatosImportantesController < ApplicationController
   # GET /contatos_importantes
   # GET /contatos_importantes.json
   def index
-    @contatos_importantes = ContatosImportante.all
 
     respond_to do |format|
       format.html # index.html.erb
